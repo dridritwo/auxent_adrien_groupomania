b54ccf8a4519bad38698ff5fbc6c4b138f0d8f9b
Adrien Auxent <a.auxent@gmail.com>
2021-11-06 21:47:05 +0100

    cleanup

--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const likeController = require('../controllers/like.controller');
 const auth = require('../middleware/auth.middleware');
-const Role = require('../utils/userRoles.utils');
 const awaitHandlerFactory = require('../middleware/awaitHandlerFactory.middleware');
 
 const { likeSchema } = require('../middleware/validators/likeValidator.middleware');
