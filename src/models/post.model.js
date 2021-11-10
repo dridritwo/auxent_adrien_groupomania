dbe0b3341ce7d622d3190d298f7ef1519960b9c7
dridrione <a.auxent@gmail.com>
2021-11-10 10:39:08 +0100

    fixed find all by author id for local mysql

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -91,10 +91,10 @@ class PostModel {
         const sql = `select 
         p.id, 
         u.id as author_id, 
-        (select count(pl.like_status) from post_likes pl where like_status = 1 and pl.post_id = p.id) as likes, 
-        (select count(pl.like_status) from post_likes pl where like_status = -1 and pl.post_id = p.id) as dislikes, 
-        (select pl.like_status from post_likes pl where user_id = ? and post_id = p.id) as like_status,
-        (select count(1) from comments where post_id = p.id) as comments_count,
+        count(pl1.post_likes_pk) as likes, 
+        count(pl2.post_likes_pk) as dislikes, 
+        pl3.like_status as like_status,
+        count(c.id) as comments_count,
         p.title, 
         p.text, 
         p.image_url , 
@@ -103,12 +103,17 @@ class PostModel {
         u.username as author_username
             from posts p 
             inner join users u on p.author_id = u.id
-            cross join post_likes pl
-            where ${columnSet}
+            left join comments c on p.id = c.post_id 
+            left join post_likes pl3 on user_id = ? and pl3.post_id = p.id
+            left join post_likes pl2 on pl2.like_status = -1 and pl2.post_id  = p.id
+            left join post_likes pl1 on pl1.like_status = 1 and pl1.post_id  = p.id
+            where p.${columnSet}
             group by p.id 
             order by creation_date desc 
             limit ? 
             offset ?`;
+            console.log("sql : ", sql)
+            console.log("values : ", values)
         const result = await query(sql, [...values, currentUserId, limit, offset]);
 
         // return back the first row 
